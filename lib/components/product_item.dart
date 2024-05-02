1d593bb8d9ee09a140bfc11fbdff1de984ce9cb0
Guilherme Saravi <gsaravi@psgtecnologia.com.br>
2024-05-01 21:24:36 -0400

    Implementing remove product

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/models/product.dart';
+import 'package:shop/models/product_list.dart';
 
 import '../utils/app_routes.dart';
 
@@ -27,7 +29,28 @@ class ProductItem extends StatelessWidget {
               color: Theme.of(context).primaryColor,
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (ctx) => AlertDialog(
+                          title: const Text('Tem certeza?'),
+                          content: const Text(
+                              'Tem certeza que deseja excluir o produto?'),
+                          actions: [
+                            TextButton(
+                                onPressed: () => Navigator.of(ctx).pop(false),
+                                child: const Text('Não')),
+                            TextButton(
+                                onPressed: () {
+                                  Provider.of<ProductList>(context,
+                                          listen: false)
+                                      .removeProduct(product);
+                                  Navigator.of(ctx).pop(true);
+                                },
+                                child: const Text('Sim')),
+                          ],
+                        ));
+              },
               icon: const Icon(Icons.delete),
               color: Colors.red[400],
             ),
